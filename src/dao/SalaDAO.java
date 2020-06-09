21440f7c108b9a8c5e35bdb7946d478e4bc1bf52
sergiomoya00 <43157614+sergiomoya00@users.noreply.github.com>
2020-06-08 19:59:56 -0600

    papas

--- src/dao/SalaDAO.java
+++ src/dao/SalaDAO.java
@@ -5,10 +5,48 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import modelo.Sala;
+
 /**
  *
  * @author samoy
  */
-public class SalaDAO {
+public class SalaDAO implements SalaDA {
+     
+    Conexion conexion = new Conexion();
+    Connection cin = conexion.getConnection();
+    PreparedStatement ps;
+
+    public SalaDAO() {
+    }
+
+    private static final SalaDAO instance = new SalaDAO();
+
+    public static SalaDAO getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Sala crearSala(Sala sala) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean modificarCantidad(String identificador, int cantidad, int nuevaCantidad) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Sala> cargarListaSalas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void obtenerAnfitrion(String identificador) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
